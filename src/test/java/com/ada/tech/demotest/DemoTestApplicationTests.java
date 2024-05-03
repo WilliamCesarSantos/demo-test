b9ca55daaa51cd5104bf2970397e401123d625c1
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-05-03 20:20:37 -0300

    First

---
+++ src/test/java/com/ada/tech/demotest/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.tech.demotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
