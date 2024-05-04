c80ebf8660014158e3f434ee24c817d542831f47
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-05-03 21:55:20 -0300

    Update

--- src/main/java/com/ada/tech/demotest/Calculadora.java
+++ src/main/java/com/ada/tech/demotest/Calculadora.java
@@ -16,4 +16,16 @@ public class Calculadora {
         return one.multiply(two);
     }
 
+    public BigDecimal divisao(BigDecimal one, BigDecimal two) {
+        if (two.compareTo(BigDecimal.ZERO) == 0) {
+            throw new IllegalArgumentException("Não é possivel dividir por zero");
+        }
+        return one.divide(two);
+    }
+
+    public BigDecimal percentual(BigDecimal valorTotal, BigDecimal valorPercentual) {
+        BigDecimal dividido = divisao(valorTotal, BigDecimal.valueOf(100));
+        return multiply(dividido, valorPercentual);
+    }
+
 }
--- src/test/java/com/ada/tech/demotest/CalculadoraTest.java
+++ src/test/java/com/ada/tech/demotest/CalculadoraTest.java
@@ -16,7 +16,7 @@ public class CalculadoraTest {
     }
 
     @Test
-    public void somarDoisPontoDoisMainUmPontoCincoDeveSerIgualATresPontoSete(){
+    public void somarDoisPontoDoisMainUmPontoCincoDeveSerIgualATresPontoSete() {
         BigDecimal resultado = calculadora.somar(BigDecimal.valueOf(2.2), BigDecimal.valueOf(1.5));
         if (resultado.compareTo(BigDecimal.valueOf(3.7)) != 0) {
             throw new RuntimeException("calculadora em falha.");
@@ -50,6 +50,95 @@ public class CalculadoraTest {
 
         // Conferir resultado
         if (resultado.compareTo(BigDecimal.valueOf(4)) != 0) {
+            throw new RuntimeException("Calculadora em falha");
+        }
+    }
+
+    @Test
+    public void dividir_oitoPorDois_deveSerIgualAQuatro() {
+        // Prover os dados para execução
+        BigDecimal one = BigDecimal.valueOf(8);
+        BigDecimal two = BigDecimal.valueOf(2);
+
+        // Chamar a execução
+        BigDecimal resultado = calculadora.divisao(one, two);
+
+        // Conferir resultado
+        BigDecimal valorEsperado = BigDecimal.valueOf(4);
+        if (resultado.compareTo(valorEsperado) != 0) {
+            throw new RuntimeException("Calculadora em falha");
+        }
+    }
+
+    // Dividir 8.2 por 2, deve gerar resultado igual a 4.1
+    // Valida que a divisão esteja trabalhando com número decimais
+    @Test
+    public void dividir_oitoPontoDoisPorDois_deveSerIgualAQuatroPontoUm() {
+        // Prover os dados para execução
+        BigDecimal one = BigDecimal.valueOf(8.2);
+        BigDecimal two = BigDecimal.valueOf(2);
+
+        // Chamar a execução
+        BigDecimal resultado = calculadora.divisao(one, two);
+
+        // Conferir resultado
+        BigDecimal valorEsperado = BigDecimal.valueOf(4.1);
+        if (resultado.compareTo(valorEsperado) != 0) {
+            throw new RuntimeException("Calculadora em falha");
+        }
+    }
+
+    // Dividir 8 por 2.5, deve gerar resultado igual a 3.2
+    @Test
+    public void dividir_oitoPorDoisPontoCinco_deveSerIgualATresPontoDois() {
+        // Prover os dados para execução
+        BigDecimal one = BigDecimal.valueOf(8);
+        BigDecimal two = BigDecimal.valueOf(2.5);
+
+        // Chamar a execução
+        BigDecimal resultado = calculadora.divisao(one, two);
+
+        // Conferir resultado
+        BigDecimal valorEsperado = BigDecimal.valueOf(3.2);
+        if (resultado.compareTo(valorEsperado) != 0) {
+            throw new RuntimeException("Calculadora em falha");
+        }
+    }
+
+    // Divisao não pode aceitar zero
+    @Test
+    public void dividir_doisPorZero_deveLancarExcecao() {
+        // Prover os dados para execução
+        BigDecimal one = BigDecimal.valueOf(2);
+        BigDecimal two = BigDecimal.ZERO;
+
+        // Chamar a execução
+        Boolean ocorreuExcecao = false;
+        try {
+            calculadora.divisao(one, two);
+        } catch (IllegalArgumentException exception) {
+            ocorreuExcecao = true;
+        }
+
+        //Conferir resultado
+        if (!ocorreuExcecao) {
+            throw new RuntimeException("Calculadora em falha");
+        }
+    }
+
+    // Calcular 10 porcento de 150, deve ser igual a 15
+    @Test
+    public void percentual_dezDeCentoECinquenta_deveSerIgualAQuinze() {
+        // Prover os dados para execução
+        BigDecimal valorTotal = BigDecimal.valueOf(150);
+        BigDecimal percentual = BigDecimal.valueOf(10);
+
+        // Chamar a execução
+        BigDecimal resultado = calculadora.percentual(valorTotal, percentual);
+
+        // Conferir resultado
+        BigDecimal valorEsperado = BigDecimal.valueOf(15);
+        if (resultado.compareTo(valorEsperado) != 0) {
             throw new RuntimeException("Calculadora em falha");
         }
     }
