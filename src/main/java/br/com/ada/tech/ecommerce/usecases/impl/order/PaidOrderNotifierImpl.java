da4cac01b2e570d24f9a4fd18470ffb4eeb4e9fd
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-05-06 22:02:59 -0300

    Update

--- src/main/java/br/com/ada/tech/ecommerce/usecases/impl/order/PaidOrderNotifierImpl.java
+++ src/main/java/br/com/ada/tech/ecommerce/usecases/impl/order/PaidOrderNotifierImpl.java
@@ -2,7 +2,9 @@ package br.com.ada.tech.ecommerce.usecases.impl.order;
 
 import br.com.ada.tech.ecommerce.model.Order;
 import br.com.ada.tech.ecommerce.usecases.order.IPaidOrderNotifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PaidOrderNotifierImpl implements IPaidOrderNotifier {
     @Override
     public void notify(Order order) {
